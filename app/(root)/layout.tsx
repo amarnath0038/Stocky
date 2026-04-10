5aa9d49ee6bc338056687c22a39283a504f12ae3
amarnath0038 <amarnathreddy841@gmail.com>
2026-04-10 11:26:26 +0530

    Initial commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,11 @@
+const RootLayout = ({ children }: { children : React.ReactNode}) => {
+  return (
+    <main className="min-h screen text-gray-400">
+        <div className="container py-10">
+            {children}
+        </div>
+    </main>
+  ); 
+};
+
+export default RootLayout;
