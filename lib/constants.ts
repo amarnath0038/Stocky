1006df1763140c6dd1670c4c9ad5b5083053740d
amarnath0038 <amarnathreddy841@gmail.com>
2026-04-10 14:41:35 +0530

    Created navItems list

---
+++ lib/constants.ts
@@ -0,0 +1,11 @@
+type NavItem = {
+    href: string;
+    label: string;
+} 
+
+export const navItems: NavItem[] = [
+    { href: "/", label: "Dashboard"},
+    { href: "/search", label: "Search"},
+    { href: "/watchlist", label: "Watchlist"}
+
+]
